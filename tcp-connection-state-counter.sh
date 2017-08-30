26a57d8633c995cbb27c030cdd169736dbbc814d
Chuan Zhang <bj.illusion@hotmail.com>
2017-08-30 15:15:08 +0800

    tcp-connection info

---
+++ tcp-connection-state-counter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# @Function
+# show count of tcp connection stat.
+#
+# @Usage
+#   $ ./tcp-connection-state-counter.sh
+#
+# @author 
+
+
+# On MacOS, netstat need to using -p tcp to get only tcp output.
+[ -n "$(uname | grep Darwin)" ] && option_for_mac="-ptcp"
+
+netstat -tna $option_for_mac | awk 'NR > 2 {
+    s[$NF]++
+}
+END {
+    for(v in s) {
+        printf "%-12s%s\n", v, s[v]
+    }
+}' | sort -nr -k2,2
